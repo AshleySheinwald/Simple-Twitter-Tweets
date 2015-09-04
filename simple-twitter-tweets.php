79d41a0381ca4771e2fe1616b92ad40ca2d7890d
Ashley Sheinwald <ashley@planet-interactive.co.uk>
2015-09-04 13:58:39 +0100

    ReadMe updated and files now referencing 4.0 versioning

--- simple-twitter-tweets.php
+++ simple-twitter-tweets.php
@@ -4,7 +4,7 @@ Plugin Name: Simple Twitter Tweets
 Plugin URI: http://www.planet-interactive.co.uk/simple-twitter-tweets
 Description: Display last x number tweets from Twitter API stream, store locally in database to present past tweets when failure to access Twitters restrictive API occurs
 Author: Ashley Sheinwald
-Version: 3.3
+Version: 4.0
 Author URI: http://www.planet-interactive.co.uk/
 Text Domain: simple-twitter-tweets
 */
